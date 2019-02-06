1164a38ee9405c4ed9a05ab9ed8ea127691f6968
Karl Köörna <karlkoorna@gmail.com>
2019-02-06 18:22:00 +0200

    Improve comments, request content type validation, error handling and header, footer options

--- app.js
+++ app.js
@@ -17,51 +17,55 @@ http.createServer((req, res) => {
 	// Validate request method.
 	if (req.method !== 'POST') {
 		res.statusCode = 405;
-		res.end('(405) Method Not Allowed - Only POST method supported.');
+		res.end('(405) Method Not Allowed - Only POST method allowed.');
 		return;
 	}
 	
-	// Validate content type.
-	switch (req.headers['content-type']) {
-		case 'multipart/form-data': break;
-		default:
-			res.statusCode = 400;
-			res.end('(400) Bad Request - Only Form Data supported.');
-			return;
+	// Validate request content type.
+	if (req.headers['content-type'] !== 'multipart/form-data') {
+		res.statusCode = 400;
+		res.end('(400) Bad Request - Only Multipart Form Data allowed.');
+		return;
 	}
 	
 	// Assemble request body and respond with generated PDF file.
-	let form = '';
+	let data = '';
 	req.on('data', (chunk) => {
-		form += chunk;
+		data += chunk;
 	}).on('end', async () => {
-		form = querystring.parse(form);
-		
-		res.setHeader('content-type', 'application/pdf');
-		res.setHeader('content-disposition', 'attachment');
-		
-		const ctx = await browser.createIncognitoBrowserContext();
-		const page = await ctx.newPage();
-		
-		await page.setJavaScriptEnabled(true);
-		await page.setContent(form.body);
-		
-		// Remove used keys.
-		delete form.body;
-		delete form.path;
-		
-		// Override default paper format.
-		if (!form.format && !form.width && !form.height) form.format = 'A4';
-		
-		// Transform margin options.
-		const margin = {};
-		if (form.marginLeft) margin.left = form.marginLeft;
-		if (form.marginRight) margin.right = form.marginRight;
-		if (form.marginTop) margin.top = form.marginTop;
-		if (form.marginBottom) margin.bottom = form.marginBottom;
-		form.margin = margin;
-		
-		res.end(await page.pdf(form));
+		try {
+			const form = querystring.parse(data);
+			const ctx = await browser.createIncognitoBrowserContext();
+			const page = await ctx.newPage();
+			
+			await page.setJavaScriptEnabled(true);
+			await page.setContent(form.body);
+			
+			// Transform header and footer options.
+			form.headerTemplate = form.header;
+			form.footerTemplate = form.footer;
+			
+			// Override default paper format.
+			if (!form.format && !form.width && !form.height) form.format = 'A4';
+			
+			// Transform margin options.
+			const margin = {};
+			if (form.marginLeft) margin.left = form.marginLeft;
+			if (form.marginRight) margin.right = form.marginRight;
+			if (form.marginTop) margin.top = form.marginTop;
+			if (form.marginBottom) margin.bottom = form.marginBottom;
+			form.margin = margin;
+			
+			// Prevent saving file on server.
+			delete form.path;
+			
+			res.setHeader('content-type', 'application/pdf');
+			res.setHeader('content-disposition', 'attachment');
+			res.end(await page.pdf(form));
+		} catch (ex) {
+			res.statusCode = 500;
+			res.end(`(500) Internal Server Error - ${ex.message}`);
+		}
 	});
 	
 }).listen(...LISTEN.split(':').reverse(), () => {
